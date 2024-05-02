25feca0a8939621af44708b83fd0c0daea2db44a
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-02 18:15:50 +0100

    adding css commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ const App = () => {
   return (
     <div>
       <div className="button">submit</div>
+      <h1>test</h1>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      textColor: "#252525",
+      white: "#ffffff",
+      grey: "#eee",
+      mainColor: "#096FAD",
+      hoverColor: "#439CDB",
+      secondColor: "#69fab4",
+    },
+    extend: {
+      fontFamily: {
+        main: ["Jost", "sans-serif"],
+        second: ["Lora", "serif"],
+      },
+    },
   },
   plugins: [],
 };
