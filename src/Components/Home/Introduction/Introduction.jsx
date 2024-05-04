e90be418c138f2c1cd29ba3f927629069d10a603
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-04 20:03:42 +0100

    adding intoduction component commit

--- src/Components/Home/Introduction/Introduction.jsx
+++ src/Components/Home/Introduction/Introduction.jsx
@@ -3,10 +3,18 @@ import Boumerdes from "../../../assets/BoumerdesTwo.png";
 const Introduction = () => {
   return (
     <div>
-      <div>
+      <div className="relative">
         <div>
           {" "}
           <img src={Boumerdes} className="w-full" alt="" />
+        </div>
+        <div className="absolute top-1/2">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam
+            distinctio, quod, ducimus mollitia placeat quisquam, provident
+            maxime sit rem tempore ratione tempora. Ipsum voluptatem temporibus
+            cumque, voluptatibus totam magnam! Harum?
+          </p>
         </div>
       </div>
     </div>
