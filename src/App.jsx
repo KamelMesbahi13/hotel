895693912dc69741d5904e3f631325926250f755
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-04 18:05:57 +0100

    adding intoduction component commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Introduction from "./Introduction/Introduction";
+
+const Home = () => {
+  return (
+    <>
+      <div className="container"></div>
+      <Introduction />
+    </>
+  );
+};
+
+export default Home;
