d51490dd7a40d324b7a60adb28df92ad55d1fabe
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-04 15:40:26 +0100

    adding navbar commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,172 @@
+import { useState } from "react";
+import Data from "../../Data/Data.json";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/Gmail.png";
+import { AiOutlineMenu } from "react-icons/ai";
+import { FaXmark } from "react-icons/fa6";
+import Facebook from "../../assets/Facebook.png";
+import Youtube from "../../assets/Youtube.png";
+import TranslateIcon from "../../Ui/TranslationIcon/TranslationIcon";
+import { useTranslation } from "react-i18next";
+
+const Navbar = () => {
+  const { i18n } = useTranslation();
+
+  const modifiedDataNav = Data.navbarItems.map((data) => {
+    if (i18n.language === "ar") {
+      return {
+        id: data.id,
+        ItemOne: data.ItemOne_ar,
+        ItemTwo: data.ItemTwo_ar,
+        ItemThree: data.ItemThree_ar,
+        ItemFour: data.ItemFour_ar,
+      };
+    }
+
+    if (i18n.language === "fr") {
+      return {
+        id: data.id,
+        ItemOne: data.ItemOne_fr,
+        ItemTwo: data.ItemTwo_fr,
+        ItemThree: data.ItemThree_fr,
+        ItemFour: data.ItemFour_fr,
+      };
+    }
+    return data;
+  });
+
+  const [nav, setNav] = useState(true);
+
+  const navHandler = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <>
+      <div id="nav">
+        <div className="container">
+          <div className="z-50 hidden w-full py-6 lg:flex">
+            <div className="flex items-center justify-between w-full">
+              <div>
+                <a href="/">
+                  <img className="w-28" src={Logo} alt="Logo" />
+                </a>
+              </div>
+              <div className="flex gap-8">
+                <Link className="link" to="/">
+                  {modifiedDataNav.map((el) => el.ItemOne)}
+                </Link>
+                <Link className="link" to="/">
+                  {modifiedDataNav.map((el) => el.ItemTwo)}
+                </Link>
+                <Link className="link" to="/">
+                  {modifiedDataNav.map((el) => el.ItemThree)}
+                </Link>
+                <Link className="link" to="/">
+                  {modifiedDataNav.map((el) => el.ItemFour)}
+                </Link>
+              </div>
+              <div className="flex items-center">
+                <span className="text-2xl cursor-pointer ">
+                  <TranslateIcon />
+                </span>
+                <div>
+                  <a
+                    href="https://web.facebook.com/dzworkaway"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img className="w-6 mx-2" src={Facebook} alt="Facebook" />
+                  </a>
+                </div>
+                <div>
+                  <a
+                    href="https://www.instagram.com/dzworkaway"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img className="w-6" src={Youtube} alt="Instagram" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="container flex flex-row-reverse items-center justify-between lg:hidden">
+            <div className={nav ? "visible lg:invisible" : "invisible"}>
+              <img src={Logo} className="w-24" alt="Logo" />
+            </div>
+            <div>
+              <div className="lg:hidden">
+                <div className="cursor-pointer">
+                  {nav ? (
+                    <AiOutlineMenu className="w-6 h-6" onClick={navHandler} />
+                  ) : (
+                    <FaXmark className="w-6 h-6" onClick={navHandler} />
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="relative">
+            <div
+              className={
+                nav
+                  ? "absolute w-full rtl:left-[150%] opacity-0 ltr:left-[-150%] duration-500 z-50"
+                  : "absolute w-full left-0 bg-grey opacity-100 shadow-2xl z-50 duration-500"
+              }
+            >
+              <div className="flex flex-col px-8 py-16 gap-y-8">
+                <Link onClick={navHandler} to="/">
+                  {modifiedDataNav.map((el) => el.ItemOne)}
+                </Link>
+                <Link onClick={navHandler} to="/">
+                  {modifiedDataNav.map((el) => el.ItemTwo)}
+                </Link>
+                <Link onClick={navHandler} to="/">
+                  {modifiedDataNav.map((el) => el.ItemThree)}
+                </Link>
+                <Link onClick={navHandler} to="/">
+                  {modifiedDataNav.map((el) => el.ItemFour)}
+                </Link>
+
+                <div className="flex items-center justify-center gap-2">
+                  <a
+                    href="https://web.facebook.com/dzworkaway"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img className="w-5" src={Facebook} alt="Facebook" />
+                  </a>
+
+                  <a
+                    href="https://www.instagram.com/dzworkaway/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img className="w-5" src={Youtube} alt="Instagram" />
+                  </a>
+                  <div>
+                    <TranslateIcon />
+                  </div>
+                </div>
+
+                <div className="mx-auto mt-2 lg:hidden">
+                  <img
+                    className={nav ? "invisible w-24" : "visible w-24"}
+                    src={Logo}
+                    alt="Logo"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/Ui/TranslationIcon/TranslationIcon.jsx
@@ -0,0 +1,106 @@
+import { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+
+const languages = [
+  {
+    code: "fr",
+    name: "Français",
+    dir: "ltr",
+  },
+  {
+    code: "en",
+    name: "English",
+    dir: "ltr",
+  },
+  {
+    code: "ar",
+    name: "العربية",
+    dir: "rtl",
+  },
+];
+
+const GlobeIcon = ({ width = 24, height = 24 }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={width}
+    height={height}
+    fill="currentColor"
+    viewBox="0 0 16 16"
+  >
+    <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z" />
+  </svg>
+);
+
+function TranslateIcon() {
+  const [isOpen, setIsOpen] = useState(false);
+  const currentLanguageCode = localStorage.getItem("i18next") || "fr";
+  const currentLanguage = languages.find((l) => l.code === currentLanguageCode);
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    i18n.changeLanguage(currentLanguageCode);
+    document.body.dir = currentLanguage.dir || "ltr";
+  }, [currentLanguage, currentLanguageCode, i18n]);
+
+  const changeLanguage = (code) => {
+    i18n.changeLanguage(code);
+    setIsOpen(false);
+    localStorage.setItem("i18next", code);
+  };
+
+  const [nav, setNav] = useState(true);
+
+  const navHandler = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div>
+      <div className="language-select">
+        <div className="flex items-center justify-end language-select-root">
+          <div className="relative">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="flex items-center space-x-1 text-sm font-medium"
+            >
+              <GlobeIcon />
+            </button>
+            {isOpen && (
+              <ul className="absolute z-10 px-8 mt-2 bg-white border-2 rounded-md shadow-2xl -right-12 border-grey">
+                <li>
+                  <h6 className="block px-2 py-1 text-lg font-bold lg:px-4 lg:py-2 text-mainColor">
+                    {t("Language")}
+                  </h6>
+                </li>
+                <div>
+                  {languages.map(({ code, name }, i) => (
+                    <li onClick={navHandler} key={i}>
+                      <button
+                        className={classNames("block px-4 py-2 text-sm", {
+                          "text-gray-500": currentLanguageCode === code,
+                          "text-gray-900": currentLanguageCode !== code,
+                        })}
+                        onClick={() => changeLanguage(code)}
+                      >
+                        {name}
+                      </button>
+                    </li>
+                  ))}
+                </div>
+              </ul>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+GlobeIcon.propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number,
+};
+
+export default TranslateIcon;
