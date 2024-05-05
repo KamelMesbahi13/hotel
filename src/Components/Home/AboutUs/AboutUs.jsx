247b72136fdf224a826240ea55c541f175768120
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-05 20:42:07 +0100

    adding about us component commit

--- src/Components/Home/AboutUs/AboutUs.jsx
+++ src/Components/Home/AboutUs/AboutUs.jsx
@@ -1,9 +1,19 @@
+import RoomOne from "../../../assets/RoomOne.jpg";
+import RoomTwo from "../../../assets/RoomTwo.jpg";
+
 const AboutUs = () => {
   return (
     <div>
       <div className="mt-36">
         <div>
-          <div></div>
+          <div>
+            <img src={RoomOne} className="w-[35%]" alt="" />
+            <img
+              className="absolute top-[65rem] w-[25%] left-[20rem]"
+              src={RoomTwo}
+              alt=""
+            />
+          </div>
         </div>
       </div>
     </div>
