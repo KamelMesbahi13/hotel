dae3545b976944bc78e272586094a3ec29d0742c
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-05 16:31:21 +0100

    adding mission component commit

--- src/Components/Home/Misson/Mission.jsx
+++ src/Components/Home/Misson/Mission.jsx
@@ -5,11 +5,22 @@ const Mission = () => {
 
   return (
     <>
-      <div className="mt-24 textCenter">
+      <div>
         <div>
-          <div className="w-3/4">
-            <h2 className="mb-2">{t("mission_subheader")}</h2>
-            <h1>{t("mission_header")}</h1>
+          <div className="mt-24 textCenter">
+            <div>
+              <div className="w-3/4">
+                <h2 className="mb-2">{t("mission_subheader")}</h2>
+                <h1>{t("mission_header")}</h1>
+              </div>
+            </div>
+          </div>
+          <div className="mt-28">
+            <div>
+              <div>
+                <h2>What We Offer</h2>
+              </div>
+            </div>
           </div>
         </div>
       </div>
