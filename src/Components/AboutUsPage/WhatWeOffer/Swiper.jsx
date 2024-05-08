c66bc2305d5328d6f31dc84749b0101573315758
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-08 19:32:17 +0100

    adding what we offre component commit

---
+++ src/Components/AboutUsPage/WhatWeOffer/Swiper.jsx
@@ -0,0 +1,49 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import ImgOne from "../../../assets/RoomGalleryOne.jpg";
+import ImgTwo from "../../../assets/RoomGalleryTwo.jpg";
+import ImgThree from "../../../assets/RoomGalleryThree.jpg";
+import ImgFour from "../../../assets/RoomGalleryFour.jpg";
+import ImgFive from "../../../assets/RoomGalleryFive.jpg";
+
+const Imgs = [
+  {
+    id: 1,
+    Img: ImgOne,
+  },
+  {
+    id: 2,
+    Img: ImgTwo,
+  },
+  {
+    id: 3,
+    Img: ImgThree,
+  },
+  {
+    id: 4,
+    Img: ImgFour,
+  },
+  {
+    id: 5,
+    Img: ImgFive,
+  },
+];
+
+export default function App() {
+  return (
+    <>
+      <div className="mt-12">
+        <Swiper slidesPerView={3} spaceBetween={30} className="mySwiper">
+          {Imgs.map((item) => {
+            return (
+              <SwiperSlide className="cursor-grab" key={item.id}>
+                <img src={item.Img} alt="" />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+    </>
+  );
+}
--- src/Components/AboutUsPage/WhatWeOffer/WhatWeOffer.jsx
+++ src/Components/AboutUsPage/WhatWeOffer/WhatWeOffer.jsx
@@ -1,19 +1,30 @@
+import Swiper from "./Swiper";
+
 const WhatWeOffer = () => {
   return (
-    <div>
+    <div className="mt-16">
       <div>
-        <div>
+        <div className="flex flex-col justify-between md:flex-row">
           <div>
             <h2>What we offer</h2>
             <h1>Relaxation and luxury during your stay</h1>
           </div>
-          <div>
+          <div className="mt-12 md:w-2/4 md:mt-20">
             <p>
               Relax and enjoy luxury with us. We have comfy rooms, great views,
               and a peaceful vibe just for you. Your comfort matters most!
             </p>
+            <p className="mt-8">
+              In our hotel apartment, we offer cozy rooms with kitchens, comfy
+              beds, and amenities like WiFi, parking, and security. Choose from
+              our F3, F2, and studio options. Our vision is to make you happy
+              and feel at home while exploring our city.
+            </p>
           </div>
         </div>
+      </div>
+      <div>
+        <Swiper />
       </div>
     </div>
   );
