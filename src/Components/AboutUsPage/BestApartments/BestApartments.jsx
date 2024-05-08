03982690a7699abd1fc9f4766b7a7679c3000183
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-08 11:27:44 +0100

    best apartment component commit

--- src/Components/AboutUsPage/BestApartments/BestApartments.jsx
+++ src/Components/AboutUsPage/BestApartments/BestApartments.jsx
@@ -6,19 +6,19 @@ const BestApartments = () => {
 
   return (
     <div>
-      <div className="mt-12 md:mt-36">
-        <div className="flex flex-col justify-between md:flex-row">
-          <div className="mt-0 md:mt-24 md:w-1/2 md:ltr:mr-8">
+      <div className="mt-12 lg:mt-36">
+        <div className="flex flex-col justify-between lg:flex-row">
+          <div className="mt-0 lg:mt-24 lg:w-1/2 lg:ltr:mr-8">
             <h2>{t("best_apartments_subheader")}</h2>
             <h1 className="my-4">{t("best_apartments_header")} </h1>
             <p>{t("best_apartments_paragraph")}</p>
           </div>
-          <div className="mt-8 md:mt-0">
+          <div className="mt-8 lg:mt-0">
             <div>
               <div>
                 <img src={Deco} alt="Deco" />
               </div>
-              <div className="hidden md:block">
+              <div className="hidden lg:block">
                 <div className="-z-10 bg-secondColor relative bottom-[29rem] h-[27rem] w-[30rem]"></div>
               </div>
             </div>
