3b459cedea0ee50a51bbe8d93547e5c50ca9ab91
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-08 11:07:59 +0100

    best apartment component commit

--- src/Components/AboutUsPage/BestApartments/BestApartments.jsx
+++ src/Components/AboutUsPage/BestApartments/BestApartments.jsx
@@ -1,26 +1,22 @@
 import Deco from "../../../assets/AboutUsImgOne.jpg";
+import { useTranslation } from "react-i18next";
 
 const BestApartments = () => {
+  const { t } = useTranslation();
+
   return (
     <div>
       <div className="mt-12 md:mt-36">
         <div className="flex flex-col justify-between md:flex-row">
           <div className="mt-0 md:mt-24 md:w-1/2">
-            <h2>Luxury property</h2>
-            <h1 className="my-4">
-              We offer the best hotel apartments in the city.
-            </h1>
-            <p>
-              Discover our fancy hotel apartments for a cozy and luxurious stay,
-              whether you re here for work or fun. Our rooms are comfy, our
-              amenities are modern, and our vibe is friendly. Get ready for
-              awesome hospitality and make great memories with us!
-            </p>
+            <h2>{t("best_apartments_subheader")}</h2>
+            <h1 className="my-4">{t("best_apartments_header")} </h1>
+            <p>{t("best_apartments_paragraph")}</p>
           </div>
           <div className="mt-8 md:mt-0">
             <div>
               <div>
-                <img src={Deco} alt="" />
+                <img src={Deco} alt="Deco" />
               </div>
               <div className="hidden md:block">
                 <div className="-z-10 bg-secondColor relative bottom-[29rem] h-[27rem] w-[30rem]"></div>
