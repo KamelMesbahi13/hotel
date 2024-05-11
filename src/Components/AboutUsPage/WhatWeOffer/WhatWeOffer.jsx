91d9031b8215443f79e57e461bcdd5402f3c37fd
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-11 21:30:26 +0100

    fixing some styling commit

--- src/Components/AboutUsPage/WhatWeOffer/WhatWeOffer.jsx
+++ src/Components/AboutUsPage/WhatWeOffer/WhatWeOffer.jsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "react-i18next";
 import Swiper from "./Swiper";
 import { motion } from "framer-motion";
+import Deco from "../../../assets/DecoTwo.png";
 
 const WhatWeOffer = () => {
   const { t } = useTranslation();
@@ -17,16 +18,21 @@ const WhatWeOffer = () => {
             visible: { opacity: 1, y: 0 },
           }}
         >
-          <div className="flex flex-col justify-between md:flex-row">
-            <div>
-              <h2>{t("what_we_offer_subheader_about_us")}</h2>
-              <h1>{t("what_we_offer_header_about_us")}</h1>
+          <div>
+            <div className="flex flex-col justify-between md:flex-row">
+              <div>
+                <h2>{t("what_we_offer_subheader_about_us")}</h2>
+                <h1>{t("what_we_offer_header_about_us")}</h1>
+              </div>
+              <div className="mt-12 md:w-2/4 md:mt-20">
+                <p>{t("what_we_offer_paragraph_one_about_us")}</p>
+                <p className="mt-8">
+                  {t("what_we_offer_paragraph_two_about_us")}
+                </p>
+              </div>
             </div>
-            <div className="mt-12 md:w-2/4 md:mt-20">
-              <p>{t("what_we_offer_paragraph_one_about_us")}</p>
-              <p className="mt-8">
-                {t("what_we_offer_paragraph_two_about_us")}
-              </p>
+            <div>
+              <img src={Deco} alt="Deco" />
             </div>
           </div>
         </motion.div>
