024ca7da82de1cdc636b7b952401d9eeb7b378c9
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-11 15:47:55 +0100

    adding reservation component commit

--- src/Components/ContactUs/Reservation/Reservation.jsx
+++ src/Components/ContactUs/Reservation/Reservation.jsx
@@ -9,19 +9,20 @@ const Reservation = () => {
     <div className="mt-20">
       <div className="reservationBack">
         <div className="container">
-          <motion.div
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true, amount: 0.5 }}
-            transition={{ duration: 0.9 }}
-            variants={{
-              hidden: { opacity: 0, y: 25 },
-              visible: { opacity: 1, y: 0 },
-            }}
-          >
+          <div>
             <div className="flex flex-col py-40 justify-evenly md:flex-row">
               <div>
-                <div className="textCenter md:initialDirection">
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ duration: 0.9 }}
+                  variants={{
+                    hidden: { opacity: 0, y: 250 },
+                    visible: { opacity: 1, y: 0 },
+                  }}
+                  className="textCenter md:initialDirection"
+                >
                   <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3196.669850087001!2d3.4540857746309466!3d36.75449497027669!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x128e685e3bdd12a1%3A0x9b134c10fe59d691!2sR%C3%A9sidence%20Zidane%2C%20Lotissement%20A%C3%AFn%20Abdellah!5e0!3m2!1sen!2sdz!4v1715088631450!5m2!1sen!2sdz"
                     allowFullScreen={true}
@@ -30,9 +31,19 @@ const Reservation = () => {
                     title="Google Maps"
                     className="md:w-[600px] md:h-[450px] w-[300px] h-[250px] border-[10px] border-white"
                   ></iframe>
-                </div>
+                </motion.div>
               </div>
-              <div className="px-4 py-8 mt-8 border md:mt-0 md:px-8 md:py-16 textCenter">
+              <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ duration: 0.9 }}
+                variants={{
+                  hidden: { opacity: 0, y: 25 },
+                  visible: { opacity: 1, y: 0 },
+                }}
+                className="px-4 py-8 mt-8 border md:mt-0 md:px-8 md:py-16 textCenter"
+              >
                 <div className="mb-8 md:mb-16">
                   <div className="mb-4">
                     <h2 className="text-xs md:text-sm text-secondColor">
@@ -76,9 +87,9 @@ const Reservation = () => {
                     </div>
                   </div>
                 </div>
-              </div>
+              </motion.div>
             </div>
-          </motion.div>
+          </div>
         </div>
       </div>
     </div>
