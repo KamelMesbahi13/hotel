8b4fb047ce4e7b6d319d208a4494305b0c9f944c
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-12 20:56:51 +0100

    adding f3 high standing apartment commit

--- src/Components/FThreeHighApartments/OurRooms/OurRooms.jsx
+++ src/Components/FThreeHighApartments/OurRooms/OurRooms.jsx
@@ -1,5 +1,40 @@
+import { useTranslation } from "react-i18next";
+
 const OurRooms = () => {
-  return <div>OurRooms</div>;
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      <div className="md:mt-56">
+        <div>
+          <div>
+            {" "}
+            <div className="mt-8 md:mt-64 md:textCenter">
+              <div>
+                <div className="md:w-3/4 md:mx-auto textCenter">
+                  <h2 className="mb-2">
+                    {t("f3_high_standing_space_our_rooms_subheader")}
+                  </h2>
+                  <h1 className="mb-4">
+                    {t("f3_high_standing_space_our_rooms_header")}
+                  </h1>
+                  <div className="flex justify-center md:mt-2">
+                    <div className="w-8 h-1 md:w-12 bg-secondColor"></div>
+                    <div className="flex ml-1">
+                      <div className="w-[3px] h1 bg-secondColor"></div>
+                      <div className="w-[3px] mx-1 bg-secondColor"></div>
+                      <div className="w-[3px] h1 bg-secondColor"></div>
+                    </div>
+                    <div className="w-8 h-1 md:w-12 bg-secondColor"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OurRooms;
