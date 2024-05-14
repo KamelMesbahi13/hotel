ac262669f3e01caf3cb205a0b337d7b137f8a598
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-14 16:28:01 +0100

    adding f3 high standing apartment details commit

--- src/Components/AllApartments/FThreeHighApartments/FThreeHighApartmentsDetails/FThreeHighApartmentsDetails.jsx
+++ src/Components/AllApartments/FThreeHighApartments/FThreeHighApartmentsDetails/FThreeHighApartmentsDetails.jsx
@@ -2,8 +2,16 @@
 // import { motion } from "framer-motion";
 // import { useTranslation } from "react-i18next";
 
+import FThreeHighApartmentsDetailsHeader from "../FThreeHighApartmentsDetailsHeader/FThreeHighApartmentsDetailsHeader";
+
 const FThreeHighApartmentsDetails = () => {
-  return <div>FThreeHighApartmentsDetails</div>;
+  return (
+    <div>
+      <div>
+        <FThreeHighApartmentsDetailsHeader />
+      </div>
+    </div>
+  );
 };
 
 export default FThreeHighApartmentsDetails;
---
+++ src/Components/AllApartments/FThreeHighApartments/FThreeHighApartmentsDetailsHeader/FThreeHighApartmentsDetailsHeader.jsx
@@ -0,0 +1,18 @@
+const FThreeHighApartmentsDetailsHeader = () => {
+  return (
+    <div>
+      <div className="w-full bg-white text-backgroundColor">
+        <div className="container">
+          <div className="py-20">
+            <div className="flex justify-between">
+              <h1>The Name Of The Apartment</h1>
+              <h6>5000 da</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FThreeHighApartmentsDetailsHeader;
