7f82592fb49cf0ea2fba08cd90cb9a991e2a8884
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-14 17:27:30 +0100

    adding f3 high standing apartment details commit

--- src/Components/AllApartments/FThreeHighApartments/FThreeHighApartmentsDetails/FThreeHighApartmentsDetails.jsx
+++ src/Components/AllApartments/FThreeHighApartments/FThreeHighApartmentsDetails/FThreeHighApartmentsDetails.jsx
@@ -1,14 +1,47 @@
-// import { useParams } from "react-router-dom";
-// import { motion } from "framer-motion";
-// import { useTranslation } from "react-i18next";
-
 import FThreeHighApartmentsDetailsHeader from "../FThreeHighApartmentsDetailsHeader/FThreeHighApartmentsDetailsHeader";
+import FThreeHighApartmentDetailsSwiper from "./FThreeHighApartmentDetailsSwiper";
+
+import { useTranslation } from "react-i18next";
+
+import Boxes from "../Data";
+import { useParams } from "react-router-dom";
 
 const FThreeHighApartmentsDetails = () => {
+  const { i18n } = useTranslation();
+
+  const modifiedData = Boxes.map((item) => {
+    if (i18n.language == "en") {
+      return {
+        id: item.id,
+        title: item.title_en,
+        price: item.price,
+      };
+    }
+
+    if (i18n.language == "ar") {
+      return {
+        id: item.id,
+        title: item.title_ar,
+        price: item.price,
+      };
+    }
+
+    return item;
+  });
+
+  const { FTroisHighStandingId } = useParams();
+
+  const Data = modifiedData.find((item) => item.id == FTroisHighStandingId);
+
   return (
     <div>
       <div>
         <FThreeHighApartmentsDetailsHeader />
+      </div>
+      <div className="container">
+        <div>
+          <FThreeHighApartmentDetailsSwiper />
+        </div>
       </div>
     </div>
   );
