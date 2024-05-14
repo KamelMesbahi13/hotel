bc745627766beaabe1459f5f0e16bc23de82fd19
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-14 18:06:12 +0100

    adding f3 high standing apartment details commit

--- src/Components/AllApartments/FThreeHighApartments/FThreeHighApartmentsDetails/FThreeHighApartmentDetailsSwiper.jsx
+++ src/Components/AllApartments/FThreeHighApartments/FThreeHighApartmentsDetails/FThreeHighApartmentDetailsSwiper.jsx
@@ -1,23 +1,46 @@
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 
+import RoomOne from "../../../../assets/fThreeHighStandingOne.jpg";
+import RoomTwo from "../../../../assets/fThreeHighStandingTwo.jpg";
+import RoomThree from "../../../../assets/fThreeHighStandingThree.jpg";
+import RoomFour from "../../../../assets/fThreeHighStandingFour.jpg";
+import RoomFive from "../../../../assets/fThreeHighStandingFive.jpg";
+import RoomSix from "../../../../assets/fThreeHighStandingSix.jpg";
+
 const images = [
   {
-    original: "https://picsum.photos/id/1018/1000/600/",
-    thumbnail: "https://picsum.photos/id/1018/250/150/",
+    original: RoomOne,
+    thumbnail: RoomOne,
   },
   {
-    original: "https://picsum.photos/id/1015/1000/600/",
-    thumbnail: "https://picsum.photos/id/1015/250/150/",
+    original: RoomTwo,
+    thumbnail: RoomTwo,
   },
   {
-    original: "https://picsum.photos/id/1019/1000/600/",
-    thumbnail: "https://picsum.photos/id/1019/250/150/",
+    original: RoomThree,
+    thumbnail: RoomThree,
+  },
+  {
+    original: RoomFour,
+    thumbnail: RoomFour,
+  },
+  {
+    original: RoomFive,
+    thumbnail: RoomFive,
+  },
+  {
+    original: RoomSix,
+    thumbnail: RoomSix,
   },
 ];
 
 const MyGallery = () => {
-  return <ImageGallery showPlayButton={false} items={images} />;
+  return (
+    <div className="rounded-images">
+      <ImageGallery showPlayButton={false} items={images} />
+    </div>
+  );
 };
 
 export default MyGallery;
--- src/Components/AllApartments/FThreeHighApartments/FThreeHighApartmentsDetails/FThreeHighApartmentsDetails.jsx
+++ src/Components/AllApartments/FThreeHighApartments/FThreeHighApartmentsDetails/FThreeHighApartmentsDetails.jsx
@@ -5,8 +5,25 @@ import { useTranslation } from "react-i18next";
 
 import Boxes from "../Data";
 import { useParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const FThreeHighApartmentsDetails = () => {
+  const { t } = useTranslation();
+
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (e) => {
+    const isValid = await trigger();
+    if (!isValid) {
+      e.preventDefault();
+    }
+  };
+
+  const inputStyles = `mt-5 rounded-md text-backgroundColor focus:outline-none bg-grey px-5 py-3`;
   const { i18n } = useTranslation();
 
   const modifiedData = Boxes.map((item) => {
@@ -39,8 +56,93 @@ const FThreeHighApartmentsDetails = () => {
         <FThreeHighApartmentsDetailsHeader />
       </div>
       <div className="container">
-        <div>
-          <FThreeHighApartmentDetailsSwiper />
+        <div className="mt-20">
+          <div className="flex flex-col md:flex-row">
+            <div>
+              <FThreeHighApartmentDetailsSwiper />
+            </div>
+            <div>
+              <div>
+                <div className="w-1/2 p-8 bg-white text-backgroundColor">
+                  <div>
+                    <div>
+                      <h6>Reserve now</h6>
+                      <h1>Send us message</h1>
+                    </div>
+                    <div>
+                      <form
+                        target="_blank"
+                        className="mb-8 text-center md:mb-0"
+                        onSubmit={onSubmit}
+                        action="#"
+                        method="POST"
+                      >
+                        <input
+                          type="text"
+                          placeholder={t("form_name")}
+                          className={inputStyles}
+                          {...register("name", {
+                            required: true,
+                            minLength: 3,
+                            maxLength: 100,
+                          })}
+                        />
+                        {errors.name && (
+                          <p className="mt-1 text-mainColor">
+                            {errors.name.type === "required" &&
+                              `${t("required")}`}
+                            {errors.name.type === "maxLength" &&
+                              `${t("maxLengthOne")}`}
+                            {errors.name &&
+                              errors.name.type === "minLength" &&
+                              `${t("minLengthOne")}`}
+                          </p>
+                        )}
+                        <input
+                          type="text"
+                          placeholder={t("form_email")}
+                          className={inputStyles}
+                          {...register("email", {
+                            required: true,
+                            pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                          })}
+                        />
+                        {errors.email && (
+                          <p className="mt-1 text-mainColor">
+                            {errors.email.type === "required" &&
+                              `${t("required")}`}
+                            {errors.email.type === "pattern" && `${t("email")}`}
+                          </p>
+                        )}
+                        <textarea
+                          placeholder={t("form_message")}
+                          className={inputStyles}
+                          rows={4}
+                          cols={20}
+                          {...register("message", {
+                            required: true,
+                            minLength: 5,
+                            maxLength: 2000,
+                          })}
+                        />
+                        {errors.message && (
+                          <p className="mt-1 text-mainColor">
+                            {errors.message.type === "required" &&
+                              `${t("required")}`}
+                          </p>
+                        )}
+                        <div className="mt-8">
+                          <button className="buttonContactUs">
+                            {t("button_three")}
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
