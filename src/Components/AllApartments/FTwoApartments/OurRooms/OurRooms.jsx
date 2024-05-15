1a8ad18b40c2d1edb2847663c25441939ac8d020
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-15 18:14:30 +0100

    adding f2 apartment details commit

--- src/Components/AllApartments/FTwoApartments/OurRooms/OurRooms.jsx
+++ src/Components/AllApartments/FTwoApartments/OurRooms/OurRooms.jsx
@@ -4,160 +4,7 @@ import { useNavigate } from "react-router-dom";
 import Boxes from "./../Data";
 
 const OurRooms = () => {
-  const Boxes = [
-    {
-      id: 1,
-      img: RoomOne,
-      stats: "5000da / 100m²",
-      button: "Plus de détails",
-      button_en: "More details",
-      button_ar: "المزيد من التفاصيل",
-      new: "Nouveau",
-      new_en: "New",
-      new_ar: "جديد",
-      price: "5000da",
-      day: "Jour",
-      day_en: "Day",
-      day_ar: "يوم",
-      room: "2 chambres",
-      room_en: "2 rooms",
-      room_ar: "2 غرف",
-      bath: "1 salle de bain",
-      bath_en: "1 bethroom",
-      bath_ar: "1 حمام",
-      title: "Maisons de luxe extraordinaires.",
-      title_en: "Extraordinary Luxury Homes.",
-      title_ar: " شقق راقية.",
-    },
-
-    {
-      id: 2,
-      img: RoomTwo,
-      stats: "5000da / 100m²",
-      button: "Plus de détails",
-      button_en: "More details",
-      button_ar: "المزيد من التفاصيل",
-      new: "Nouveau",
-      new_en: "New",
-      new_ar: "جديد",
-      price: "5000da",
-      day: "Jour",
-      day_en: "Day",
-      day_ar: "يوم",
-      room: "2 chambres",
-      room_en: "2 rooms",
-      room_ar: "2 غرف",
-      bath: "1 salle de bain",
-      bath_en: "1 bethroom",
-      bath_ar: "1 حمام",
-      title: "Maisons de luxe extraordinaires.",
-      title_en: "Extraordinary Luxury Homes.",
-      title_ar: " شقق راقية.",
-    },
-
-    {
-      id: 3,
-      img: RoomThree,
-      stats: "5000da / 100m²",
-      button: "Plus de détails",
-      button_en: "More details",
-      button_ar: "المزيد من التفاصيل",
-      new: "Nouveau",
-      new_en: "New",
-      new_ar: "جديد",
-      price: "5000da",
-      day: "Jour",
-      day_en: "Day",
-      day_ar: "يوم",
-      room: "2 chambres",
-      room_en: "2 rooms",
-      room_ar: "2 غرف",
-      bath: "1 salle de bain",
-      bath_en: "1 bethroom",
-      bath_ar: "1 حمام",
-      title: "Maisons de luxe extraordinaires.",
-      title_en: "Extraordinary Luxury Homes.",
-      title_ar: " شقق راقية.",
-    },
-
-    {
-      id: 4,
-      img: RoomFour,
-      stats: "5000da / 100m²",
-      button: "Plus de détails",
-      button_en: "More details",
-      button_ar: "المزيد من التفاصيل",
-      new: "Nouveau",
-      new_en: "New",
-      new_ar: "جديد",
-      price: "5000da",
-      day: "Jour",
-      day_en: "Day",
-      day_ar: "يوم",
-      room: "2 chambres",
-      room_en: "2 rooms",
-      room_ar: "2 غرف",
-      bath: "1 salle de bain",
-      bath_en: "1 bethroom",
-      bath_ar: "1 حمام",
-      title: "Maisons de luxe extraordinaires.",
-      title_en: "Extraordinary Luxury Homes.",
-      title_ar: " شقق راقية.",
-    },
-
-    {
-      id: 5,
-      img: RoomFive,
-      stats: "5000da / 100m²",
-      link: "fTroisAppartements",
-      button: "Plus de détails",
-      button_en: "More details",
-      button_ar: "المزيد من التفاصيل",
-      new: "Nouveau",
-      new_en: "New",
-      new_ar: "جديد",
-      price: "5000da",
-      day: "Jour",
-      day_en: "Day",
-      day_ar: "يوم",
-      room: "2 chambres",
-      room_en: "2 rooms",
-      room_ar: "2 غرف",
-      bath: "1 salle de bain",
-      bath_en: "1 bethroom",
-      bath_ar: "1 حمام",
-      title: "Maisons de luxe extraordinaires.",
-      title_en: "Extraordinary Luxury Homes.",
-      title_ar: " شقق راقية.",
-    },
-
-    {
-      id: 6,
-      img: RoomSix,
-      stats: "5000da / 100m²",
-      link: "fTroisAppartements",
-      button: "Plus de détails",
-      button_en: "More details",
-      button_ar: "المزيد من التفاصيل",
-      new: "Nouveau",
-      new_en: "New",
-      new_ar: "جديد",
-      price: "5000da",
-      day: "Jour",
-      day_en: "Day",
-      day_ar: "يوم",
-      room: "2 chambres",
-      room_en: "2 rooms",
-      room_ar: "2 غرف",
-      bath: "1 salle de bain",
-      bath_en: "1 bethroom",
-      bath_ar: "1 حمام",
-      title: "Maisons de luxe extraordinaires.",
-      title_en: "Extraordinary Luxury Homes.",
-      title_ar: " شقق راقية.",
-    },
-  ];
-
+  const navigate = useNavigate();
   const { i18n } = useTranslation();
 
   const modifiedData = Boxes.map((data) => {
@@ -192,6 +39,11 @@ const OurRooms = () => {
   });
 
   const { t } = useTranslation();
+
+  const handleLinkClick = (route) => {
+    navigate(route);
+    window.scrollTo(0, 0);
+  };
 
   return (
     <div>
@@ -248,7 +100,10 @@ const OurRooms = () => {
                             <span className="text-sm"> / {item.day}</span>
                           </div>
                           <div className="mt-4">
-                            <Link to="">
+                            <Link
+                              to={`/FDeuxAppartements/${item.id}`}
+                              onClick={handleLinkClick}
+                            >
                               <button className="buttonContactUs">
                                 {item.button}
                               </button>
