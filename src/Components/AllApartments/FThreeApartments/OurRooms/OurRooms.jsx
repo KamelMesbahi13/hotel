1f35a485afa52466f256fe854cbd36c7fe233558
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-15 16:33:25 +0100

    adding f3 apartment details commit

--- src/Components/AllApartments/FThreeApartments/OurRooms/OurRooms.jsx
+++ src/Components/AllApartments/FThreeApartments/OurRooms/OurRooms.jsx
@@ -1,167 +1,11 @@
 import { useTranslation } from "react-i18next";
-import RoomOne from "../../../../assets/fThreeHighStandingOne.jpg";
-import RoomTwo from "../../../../assets/fThreeHighStandingTwo.jpg";
-import RoomThree from "../../../../assets/fThreeHighStandingThree.jpg";
-import RoomFour from "../../../../assets/fThreeHighStandingFour.jpg";
-import RoomFive from "../../../../assets/fThreeHighStandingFive.jpg";
-import RoomSix from "../../../../assets/fThreeHighStandingSix.jpg";
+import Boxes from "./../Data";
+import { useNavigate } from "react-router-dom";
+
 import { Link } from "react-router-dom";
 
 const OurRooms = () => {
-  const Boxes = [
-    {
-      id: 1,
-      img: RoomOne,
-      stats: "5000da / 100m²",
-      button: "Plus de détails",
-      button_en: "More details",
-      button_ar: "المزيد من التفاصيل",
-      new: "Nouveau",
-      new_en: "New",
-      new_ar: "جديد",
-      price: "5000da",
-      day: "Jour",
-      day_en: "Day",
-      day_ar: "يوم",
-      room: "3 chambres",
-      room_en: "3 rooms",
-      room_ar: "3 غرف",
-      bath: "1 salle de bain",
-      bath_en: "1 bethroom",
-      bath_ar: "1 حمام",
-      title: "Maisons de luxe extraordinaires.",
-      title_en: "Extraordinary Luxury Homes.",
-      title_ar: " شقق راقية.",
-    },
-
-    {
-      id: 2,
-      img: RoomTwo,
-      stats: "5000da / 100m²",
-      button: "Plus de détails",
-      button_en: "More details",
-      button_ar: "المزيد من التفاصيل",
-      new: "Nouveau",
-      new_en: "New",
-      new_ar: "جديد",
-      price: "5000da",
-      day: "Jour",
-      day_en: "Day",
-      day_ar: "يوم",
-      room: "3 chambres",
-      room_en: "3 rooms",
-      room_ar: "3 غرف",
-      bath: "1 salle de bain",
-      bath_en: "1 bethroom",
-      bath_ar: "1 حمام",
-      title: "Maisons de luxe extraordinaires.",
-      title_en: "Extraordinary Luxury Homes.",
-      title_ar: " شقق راقية.",
-    },
-
-    {
-      id: 3,
-      img: RoomThree,
-      stats: "5000da / 100m²",
-      button: "Plus de détails",
-      button_en: "More details",
-      button_ar: "المزيد من التفاصيل",
-      new: "Nouveau",
-      new_en: "New",
-      new_ar: "جديد",
-      price: "5000da",
-      day: "Jour",
-      day_en: "Day",
-      day_ar: "يوم",
-      room: "3 chambres",
-      room_en: "3 rooms",
-      room_ar: "3 غرف",
-      bath: "1 salle de bain",
-      bath_en: "1 bethroom",
-      bath_ar: "1 حمام",
-      title: "Maisons de luxe extraordinaires.",
-      title_en: "Extraordinary Luxury Homes.",
-      title_ar: " شقق راقية.",
-    },
-
-    {
-      id: 4,
-      img: RoomFour,
-      stats: "5000da / 100m²",
-      button: "Plus de détails",
-      button_en: "More details",
-      button_ar: "المزيد من التفاصيل",
-      new: "Nouveau",
-      new_en: "New",
-      new_ar: "جديد",
-      price: "5000da",
-      day: "Jour",
-      day_en: "Day",
-      day_ar: "يوم",
-      room: "3 chambres",
-      room_en: "3 rooms",
-      room_ar: "3 غرف",
-      bath: "1 salle de bain",
-      bath_en: "1 bethroom",
-      bath_ar: "1 حمام",
-      title: "Maisons de luxe extraordinaires.",
-      title_en: "Extraordinary Luxury Homes.",
-      title_ar: " شقق راقية.",
-    },
-
-    {
-      id: 5,
-      img: RoomFive,
-      stats: "5000da / 100m²",
-      link: "fTroisAppartements",
-      button: "Plus de détails",
-      button_en: "More details",
-      button_ar: "المزيد من التفاصيل",
-      new: "Nouveau",
-      new_en: "New",
-      new_ar: "جديد",
-      price: "5000da",
-      day: "Jour",
-      day_en: "Day",
-      day_ar: "يوم",
-      room: "3 chambres",
-      room_en: "3 rooms",
-      room_ar: "3 غرف",
-      bath: "1 salle de bain",
-      bath_en: "1 bethroom",
-      bath_ar: "1 حمام",
-      title: "Maisons de luxe extraordinaires.",
-      title_en: "Extraordinary Luxury Homes.",
-      title_ar: " شقق راقية.",
-    },
-
-    {
-      id: 6,
-      img: RoomSix,
-      stats: "5000da / 100m²",
-      link: "fTroisAppartements",
-      button: "Plus de détails",
-      button_en: "More details",
-      button_ar: "المزيد من التفاصيل",
-      new: "Nouveau",
-      new_en: "New",
-      new_ar: "جديد",
-      price: "5000da",
-      day: "Jour",
-      day_en: "Day",
-      day_ar: "يوم",
-      room: "3 chambres",
-      room_en: "3 rooms",
-      room_ar: "3 غرف",
-      bath: "1 salle de bain",
-      bath_en: "1 bethroom",
-      bath_ar: "1 حمام",
-      title: "Maisons de luxe extraordinaires.",
-      title_en: "Extraordinary Luxury Homes.",
-      title_ar: " شقق راقية.",
-    },
-  ];
-
+  const navigate = useNavigate();
   const { i18n } = useTranslation();
 
   const modifiedData = Boxes.map((data) => {
@@ -196,6 +40,11 @@ const OurRooms = () => {
   });
 
   const { t } = useTranslation();
+
+  const handleLinkClick = (route) => {
+    navigate(route);
+    window.scrollTo(0, 0);
+  };
 
   return (
     <div>
@@ -252,7 +101,10 @@ const OurRooms = () => {
                             <span className="text-sm"> / {item.day}</span>
                           </div>
                           <div className="mt-4">
-                            <Link to="">
+                            <Link
+                              onClick={handleLinkClick}
+                              to={`/FTroisAppartements/${item.id}`}
+                            >
                               <button className="buttonContactUs">
                                 {item.button}
                               </button>
