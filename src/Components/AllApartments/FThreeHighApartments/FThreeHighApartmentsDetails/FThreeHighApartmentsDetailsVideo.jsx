b079281b37f48ac9cb74ddb3c616af01e799c3a9
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-15 15:23:59 +0100

    adding f3 high standing apartment details commit

--- src/Components/AllApartments/FThreeHighApartments/FThreeHighApartmentsDetails/FThreeHighApartmentsDetailsVideo.jsx
+++ src/Components/AllApartments/FThreeHighApartments/FThreeHighApartmentsDetails/FThreeHighApartmentsDetailsVideo.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 const VideoPlayer = ({ videoUrl }) => {
   return (
-    <div className="mt-24">
+    <div className="mt-12 md:mt-24">
       <ReactPlayer url={videoUrl} controls width="100%" height="100%" />
     </div>
   );
