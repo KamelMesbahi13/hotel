6ecce8da3b65102e030a6ee50b6e5966b71805af
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-16 16:56:01 +0100

    fixing some code commit

--- src/Components/AllApartments/StudioApartments/OurRooms/OurRooms.jsx
+++ src/Components/AllApartments/StudioApartments/OurRooms/OurRooms.jsx
@@ -1,5 +1,5 @@
 import { useTranslation } from "react-i18next";
-import Boxes from "./../Data";
+import Boxes from "../Data";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
