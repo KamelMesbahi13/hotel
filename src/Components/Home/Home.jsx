da41e1374594ea73d2b5b0c35ce9bf3263a19cc7
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-05-18 14:14:05 +0100

    adding title commit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,6 @@
 import { lazy } from "react";
+import { useTranslation } from "react-i18next";
+import Title from "../../GeneralFunctions/Title";
 const Introduction = lazy(() => import("./Introduction/Introduction"));
 const AboutUs = lazy(() => import("./AboutUs/AboutUs"));
 const Mission = lazy(() => import("./Misson/Mission"));
@@ -9,6 +11,10 @@ const ContactUsSection = lazy(() =>
 const FindUs = lazy(() => import("./FindUs/FindUs"));
 
 const Home = () => {
+  const { t } = useTranslation();
+
+  Title(`H.A.Z  | ${t("home")}`);
+
   return (
     <>
       <Introduction />
