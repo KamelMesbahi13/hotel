2f8e70084fe547b9cdddbe30197f02d43d14c522
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-09-11 03:14:24 +0100

    Adding The Images

--- src/Components/AllApartments/FTwoApartments/ImagesData.js
+++ src/Components/AllApartments/FTwoApartments/ImagesData.js
@@ -2,7 +2,7 @@ import RoomOne from "../../../assets/F3ImgTenOff.webp";
 import RoomTwo from "../../../assets/F3ImgSevenOff.webp";
 import RoomThree from "../../../assets/F3ImgFiveOff.webp";
 import RoomFour from "../../../assets/F3ImgThreeOff.webp";
-import RoomFive from "../../../assets/F3ImgTwoOff.webp";
+import RoomFive from "../../../assets/HomeApartmentThree.webp";
 import RoomSix from "../../../assets/F3ImgElevenOff.webp";
 
 const ImagesData = [
