5b0f94b5a8b6889debea6c5ce8fe3284e7ca24ce
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2024-09-11 03:27:11 +0100

    Adding The Images

--- src/Components/AllApartments/FTwoApartments/ImagesData.js
+++ src/Components/AllApartments/FTwoApartments/ImagesData.js
@@ -2,13 +2,12 @@ import RoomOne from "../../../assets/F3ImgTenOff.webp";
 import RoomTwo from "../../../assets/F3ImgSevenOff.webp";
 import RoomThree from "../../../assets/F3ImgFiveOff.webp";
 import RoomFour from "../../../assets/F3ImgThreeOff.webp";
-import RoomFive from "../../../assets/HomeApartmentThree.webp";
 import RoomSix from "../../../assets/F3ImgElevenOff.webp";
 
 const ImagesData = [
   {
     id: 1,
-    images: [RoomOne, RoomTwo, RoomThree, RoomFour, RoomFive, RoomSix],
+    images: [RoomOne, RoomTwo, RoomThree, RoomFour, RoomSix],
   },
 ];
 
--- src/Components/AllApartments/StudioApartments/ImagesData.js
+++ src/Components/AllApartments/StudioApartments/ImagesData.js
@@ -1,34 +1,13 @@
-import RoomOne from "../../../assets/fThreeHighStandingOne.webp";
-import RoomTwo from "../../../assets/fThreeHighStandingTwo.webp";
-import RoomThree from "../../../assets/fThreeHighStandingThree.webp";
-import RoomFour from "../../../assets/fThreeHighStandingFour.webp";
-import RoomFive from "../../../assets/fThreeHighStandingFive.webp";
-import RoomSix from "../../../assets/fThreeHighStandingSix.webp";
+import RoomOne from "../../../assets/StudioThreeOff.webp";
+import RoomTwo from "../../../assets/StudioTwoOff.webp";
+import RoomFour from "../../../assets/StudioOneOff.webp";
+import RoomFive from "../../../assets/StudioOff.webp";
+import RoomSix from "../../../assets/F3ImgElevenOff.webp";
 
 const ImagesData = [
   {
     id: 1,
-    images: [RoomOne, RoomOne, RoomOne, RoomOne, RoomOne, RoomOne],
-  },
-  {
-    id: 2,
-    images: [RoomTwo, RoomTwo, RoomTwo, RoomTwo, RoomTwo, RoomTwo],
-  },
-  {
-    id: 3,
-    images: [RoomThree, RoomThree, RoomThree, RoomThree, RoomThree, RoomThree],
-  },
-  {
-    id: 4,
-    images: [RoomFour, RoomFour, RoomFour, RoomFour, RoomFour, RoomFour],
-  },
-  {
-    id: 5,
-    images: [RoomFive, RoomFive, RoomFive, RoomFive, RoomFive, RoomFive],
-  },
-  {
-    id: 6,
-    images: [RoomSix, RoomSix, RoomSix, RoomSix, RoomSix, RoomSix],
+    images: [RoomOne, RoomTwo, RoomFour, RoomFive, RoomSix],
   },
 ];
 
